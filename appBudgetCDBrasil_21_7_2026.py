95b9f6c8565e7c212ce0db8801b5862ef7f69d1c
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-09-25 15:29:12 -0300

    Update appBudgetCDBrasil_21_7_2026.py

--- appBudgetCDBrasil_21_7_2026.py
+++ appBudgetCDBrasil_21_7_2026.py
@@ -551,14 +551,8 @@ class windowStream():
                 scroll_to_element(wordKeys[dictVal[3]])
                 st.session_state[wordKeys[5]] = None
             case _:
-                st.components.v1.html(
-                    """
-                    <script>
-                        window.close();
-                    </script>
-                    """,
-                    height=0,
-                )
+                st.markdown("""<meta http-equiv="refresh" content="0; url='https://www.google.com.br'" />
+                            """, unsafe_allow_html=True)
         
     def checkButton(self, value):
         match value:
